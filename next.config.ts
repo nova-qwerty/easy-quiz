7446ede474074885e28feeeb9d593c05ce3fe865
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-02-13 13:26:37 -0500

    firebase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
 };
 
 export default nextConfig;
