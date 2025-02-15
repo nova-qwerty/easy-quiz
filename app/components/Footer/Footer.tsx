16bb4e9860e6bdae8941acfeeae5ce3301b1d198
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-02-14 19:34:32 -0500

    footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import "./styles.css";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="text-white text-center p-6 text-sm w-full mt-auto text-size absolute postion-footer">
-            <p className="mb-2">© Sens.ùs — All Rights Reserved</p>
+        <footer className="text-white text-center w-full mt-auto text-size absolute postion-footer">
+            <p>© Sens.ùs — All Rights Reserved</p>
             <p className="leading-relaxed">
                 Pettenon Cosmetics S.p.A. S.B. sede legale in via del Palù, 7/D, 35018 - San Martino di Lupari (PD - Italia)
                 <br />
@@ -13,12 +13,12 @@ const Footer: React.FC = () => {
                 <br />
                 Cap. Soc. €7.500.000,00 i.v., società soggetta a direzione e coordinamento di AGF88 Holding S.r.l.
             </p>
-            <div className="mt-4 space-x-4">
-                <a href="#" className="underline hover:text-gray-300">Privacy Policy</a>
+            <div className="mt-2 space-x-1">
+                <a href="#" className="n-underline hover:text-gray-300">Privacy Policy</a>
                 <span>|</span>
-                <a href="#" className="underline hover:text-gray-300">Cookie Policy</a>
+                <a href="#" className="n-underline hover:text-gray-300">Cookie Policy</a>
                 <span>|</span>
-                <a href="#" className="underline hover:text-gray-300">Termini d&apos;uso</a>
+                <a href="#" className="n-underline hover:text-gray-300">Termini d&apos;uso</a>
             </div>
         </footer>
     )
