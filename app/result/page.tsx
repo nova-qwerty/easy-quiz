40e3e1fa8ff91f77ab693124c133d7613365dd19
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-02-27 19:47:43 -0500

    fix result

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,165 +1,154 @@
 "use client";
-
-import React, { useState, Suspense } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 import "./styles.css";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import axios from "axios";
 
 const ResultContent: React.FC = () => {
-  const [showResult, setShowResult] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [data, setData] = useState<any>(null);
   const searchParams = useSearchParams();
-  const suggestion1 = searchParams.get("suggestion1");
-  const suggestion1Description = searchParams.get("suggestion1Description");
-  const suggestion2 = searchParams.get("suggestion2");
-  const suggestion2Description = searchParams.get("suggestion2Description");
-  const pairable1 = searchParams.get("pairable1");
-  const pairable1Description = searchParams.get("pairable1Description");
-  const pairable2 = searchParams.get("pairable2");
-  const pairable2Description = searchParams.get("pairable2Description");
-  const classHair = searchParams.get("classHair");
-  const personality = searchParams.get("personality");
+  const email = searchParams.get("email");
 
-  if (showResult) {
-    return <ResultContent />;
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!email) return; // Evitar ejecutar si no hay email
+
+      try {
+        const response = await axios.get(
+          `https://easy-quiz-361443445667.europe-west1.run.app/get-data?email=${email}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+            },
+          }
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Error obteniendo los datos:", error);
+      }
+    };
+
+    fetchData();
+  }, [email]);
 
   return (
     <div>
       <Header />
 
-      <div className="mt-6">
-        <p className="m-0 p-0 text-center text-[25px] text-[#9EA5C4]">
-          ECCO IL TUO RISULTATO
-        </p>
-        <p className="m-0 p-0 text-center text-[22px] text-[#FFFFFF]">
-          {classHair?.replace(/_/g, " ").toUpperCase()}
-        </p>
-      </div>
+      {data ? (
+        <div className="mt-6">
+          <p className="m-0 p-0 text-center text-[25px] text-[#9EA5C4]">
+            ECCO IL TUO RISULTATO
+          </p>
+          <p className="m-0 p-0 text-center text-[22px] text-[#FFFFFF]">
+            {data.classHair?.replace(/_/g, " ").toUpperCase()}
+          </p>
 
-      <div className="mt-6 relative">
-        <Image
-          src={`/result/${personality}/${classHair}/${suggestion1}.webp`}
-          alt="Logo Sensus"
-          width={1920}
-          height={300}
-        />
-        <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-          {suggestion1?.substring(1, suggestion1.length)}
-        </span>
-        <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-          {suggestion1Description}
-        </span>
-      </div>
+          <div className="mt-6 relative">
+            <Image
+              src={`/result/${data.personality}/${data.classHair}/${data.suggestions[0]}.webp`}
+              alt="Logo Sensus"
+              width={1920}
+              height={300}
+            />
+            <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
+              {data.suggestions[0]?.substring(1)}
+            </span>
+            <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
+              {data.suggestion1Description}
+            </span>
+          </div>
 
-      <div className="mt-3 relative">
-        <Image
-          src={`/result/${personality}/${classHair}/${suggestion2}.webp`}
-          alt="Logo Sensus"
-          width={1920}
-          height={300}
-        />
-        <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-          {suggestion2?.substring(1, suggestion2.length)}
-        </span>
+          <div className="mt-3 relative">
+            <Image
+              src={`/result/${data.personality}/${data.classHair}/${data.suggestions[1]}.webp`}
+              alt="Logo Sensus"
+              width={1920}
+              height={300}
+            />
+            <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
+              {data.suggestions[1]?.substring(1)}
+            </span>
+            <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
+              {data.suggestion2Description}
+            </span>
+          </div>
 
-        <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-          {suggestion2Description}
-        </span>
-      </div>
+          <div className="mt-6">
+            <p className="text-center text-[18px] text-[#FFFFFF]">
+              La tua palette è neutra. Puoi spaziare tra toni freddi e caldi,
+              ma i toni beige bilanciati e i castani sono particolarmente adatti a te.
+            </p>
+          </div>
 
-      <div className="mt-6">
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          La tua palette e neutra. Puoi spaziare tra
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          toni freddi e caldi, ma i toni beige
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          bilanciati e i castani sono
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          particolarmente adatti a te.
-        </p>
-      </div>
+          <div className="mt-6">
+            <p className="text-center text-[22px] text-[#FFFFFF]">ABBINABILI CON</p>
+          </div>
 
-      <div className="mt-6">
-        <p className="m-0 p-0 text-center text-[22px] text-[#FFFFFF]">
-          ABBINABILI CON
-        </p>
-      </div>
+          <div className="mt-6 relative">
+            <Image
+              src={`/result/${data.personality}/${data.classHair}/${data.pairable[0]}.webp`}
+              alt="Logo Sensus"
+              width={1920}
+              height={300}
+            />
+            <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
+              {data.pairable[0]?.substring(1)}
+            </span>
+            <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
+              {data.pairable1Description}
+            </span>
+          </div>
 
-      <div className="mt-6 relative">
-        <Image
-          src={`/result/${personality}/${classHair}/${pairable1}.webp`}
-          alt="Logo Sensus"
-          width={1920}
-          height={300}
-        />
-        <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-          {pairable1?.substring(1, pairable1.length)}
-        </span>
+          <div className="mt-3 relative">
+            <Image
+              src={`/result/${data.personality}/${data.classHair}/${data.pairable[1]}.webp`}
+              alt="Logo Sensus"
+              width={1920}
+              height={300}
+            />
+            <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
+              {data.pairable[1]?.substring(1)}
+            </span>
+            <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
+              {data.pairable2Description}
+            </span>
+          </div>
 
-        <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-          {pairable1Description}
-        </span>
-      </div>
+          <div className="mt-6">
+            <p className="text-center text-[18px] text-[#FFFFFF]">
+              Opta per tinte che combinano toni neutri come il beige o il naturale beige.
+              Sperimenta con outfit nei toni del grigio, sabbia e rosa antico.
+            </p>
+          </div>
 
-      <div className="mt-3 relative">
-        <Image
-          src={`/result/${personality}/${classHair}/${pairable2}.webp`}
-          alt="Logo Sensus"
-          width={1920}
-          height={300}
-        />
-        <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-          {pairable2?.substring(1, pairable2.length)}
-        </span>
+          <div className="mt-6">
+            <p className="text-center text-[18px] text-[#FFFFFF]">
+              Trova il salone più vicino a te e chiedigli di Giulietta.
+            </p>
+          </div>
 
-        <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-          {pairable2Description}
-        </span>
-      </div>
+          <div className="mt-2 text-center">
+            <button className="w-64 transition input-button text-white px-3 mt-4">
+              STORE LOCATOR
+            </button>
+          </div>
 
-      <div className="mt-6">
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          Opta per tinte che combinano toni
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          neutri come il beige o il naturale beige,
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          Sperimenta con outfit nei toni del
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          grigio, sabbia e rosa antico.
-        </p>
-      </div>
+          <div className="mt-0 mb-4 text-center">
+            <button className="w-64 transition text-white input-button-2 px-3 mt-4">
+              CONDIVIDI SUI SOCIAL
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p className="text-center text-white">Caricamento risultati...</p>
+      )}
 
-      <div className="mt-6">
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          Trova il salone piu vicino a te e
-        </p>
-        <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-          chiedigli di Giulietta
-        </p>
-      </div>
-
-      <div className="mt-2 text-center">
-        <button className="w-64 transition input-button text-white px-3 mt-4">
-          StORE LOCATOR
-        </button>
-      </div>
-
-      <div className="mt-0 mb-4 text-center">
-        <button
-          onClick={() => setShowResult(true)}
-          className="w-64 transition text-white input-button-2 px-3 mt-4"
-        >
-          CONDIVIDI SUI SOCIAL
-        </button>
-      </div>
       <Footer />
     </div>
   );
@@ -167,177 +156,10 @@ const ResultContent: React.FC = () => {
 
 const Result: React.FC = () => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<p className="text-center text-white">Loading...</p>}>
       <ResultContent />
     </Suspense>
   );
 };
 
 export default Result;
-// "use client";
-// import React, { useState } from "react";
-// import { useSearchParams } from "next/navigation";
-// import Image from "next/image";
-// import "./styles.css";
-// import Header from "../components/Header/Header";
-// import Footer from "../components/Footer/Footer";
-
-// const Result: React.FC = () => {
-//   const [showResult, setShowResult] = useState(false);
-//   const searchParams = useSearchParams();
-//   const suggestion1 = searchParams.get("suggestion1");
-//   const suggestion1Description = searchParams.get("suggestion1Description");
-//   const suggestion2 = searchParams.get("suggestion2");
-//   const suggestion2Description = searchParams.get("suggestion2Description");
-//   const pairable1 = searchParams.get("pairable1");
-//   const pairable1Description = searchParams.get("pairable1Description");
-//   const pairable2 = searchParams.get("pairable2");
-//   const pairable2Description = searchParams.get("pairable2Description");
-//   const classHair = searchParams.get("classHair");
-//   const personality = searchParams.get("personality");
-
-//   if (showResult) {
-//     return <Result />;
-//   }
-
-//   return (
-//     <div>
-//       <Header />
-
-//       <div className="mt-6">
-//         <p className="m-0 p-0 text-center text-[25px] text-[#9EA5C4]">
-//           ECCO IL TUO RISULTATO
-//         </p>
-//         <p className="m-0 p-0 text-center text-[22px] text-[#FFFFFF]">
-//           {classHair?.replace(/_/g, " ").toUpperCase()}
-//         </p>
-//       </div>
-
-//       <div className="mt-6 relative">
-//         <Image
-//           src={`/result/${personality}/${classHair}/${suggestion1}.webp`}
-//           alt="Logo Sensus"
-//           width={1920}
-//           height={300}
-//         />
-//         <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-//           {suggestion1?.substring(1, suggestion1.length)}
-//         </span>
-//         <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-//           {suggestion1Description}
-//         </span>
-//       </div>
-
-//       <div className="mt-3 relative">
-//         <Image
-//           src={`/result/${personality}/${classHair}/${suggestion2}.webp`}
-//           alt="Logo Sensus"
-//           width={1920}
-//           height={300}
-//         />
-//         <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-//           {suggestion2?.substring(1, suggestion2.length)}
-//         </span>
-
-//         <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-//           {suggestion2Description}
-//         </span>
-//       </div>
-
-//       <div className="mt-6">
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           La tua palette e neutra. Puoi spaziare tra
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           toni freddi e caldi, ma i toni beige
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           bilanciati e i castani sono
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           particolarmente adatti a te.
-//         </p>
-//       </div>
-
-//       <div className="mt-6">
-//         <p className="m-0 p-0 text-center text-[22px] text-[#FFFFFF]">
-//           ABBINABILI CON
-//         </p>
-//       </div>
-
-//       <div className="mt-6 relative">
-//         <Image
-//           src={`/result/${personality}/${classHair}/${pairable1}.webp`}
-//           alt="Logo Sensus"
-//           width={1920}
-//           height={300}
-//         />
-//         <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-//           {pairable1?.substring(1, pairable1.length)}
-//         </span>
-
-//         <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-//           {pairable1Description}
-//         </span>
-//       </div>
-
-//       <div className="mt-3 relative">
-//         <Image
-//           src={`/result/${personality}/${classHair}/${pairable2}.webp`}
-//           alt="Logo Sensus"
-//           width={1920}
-//           height={300}
-//         />
-//         <span className="absolute top-[1rem] left-[3rem] bg-[#b1b4ce] w-16 h-8 text-white text-[20px] text-center">
-//           {pairable2?.substring(1, pairable2.length)}
-//         </span>
-
-//         <span className="absolute top-[1rem] left-[8rem] text-white text-[20px] text-center">
-//           {pairable2Description}
-//         </span>
-//       </div>
-
-//       <div className="mt-6">
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           Opta per tinte che combinano toni
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           neutri come il beige o il naturale beige,
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           Sperimenta con outfit nei toni del
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           grigio, sabbia e rosa antico.
-//         </p>
-//       </div>
-
-//       <div className="mt-6">
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           Trova il salone piu vicino a te e
-//         </p>
-//         <p className="m-0 p-0 text-center text-[18px] text-[#FFFFFF]">
-//           chiedigli di Giulietta
-//         </p>
-//       </div>
-
-//       <div className="mt-2 text-center">
-//         <button className="w-64 transition input-button text-white px-3 mt-4">
-//           StORE LOCATOR
-//         </button>
-//       </div>
-
-//       <div className="mt-0 mb-4 text-center">
-//         <button
-//           onClick={() => setShowResult(true)}
-//           className="w-64 transition text-white input-button-2 px-3 mt-4"
-//         >
-//           CONDIVIDI SUI SOCIAL
-//         </button>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Result;
