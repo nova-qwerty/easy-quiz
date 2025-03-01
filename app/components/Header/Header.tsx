fcd9c4ccc4eee1a95b616e65168fa2ebb147392a
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-03-01 01:52:53 -0500

    select multi lenguaje

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,23 +1,35 @@
 "use client";
 import React, { useState } from 'react';
-import Image from "next/image"
+import Image from "next/image";
 import "./styles.css";
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [language, setLanguage] = useState("IT");
 
   return (
     <header className="text-white header-margin">
       <div className="flex justify-between items-center">
         <Image src="/logo-sensus.svg" alt="Logo Sensus" width={135} height={47.37} />
-
-        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="p-2">
-          <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16" />
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 12h16" />
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 18h16" />
-          </svg>
-        </button>
+        
+        <div className="flex items-center gap-1">
+          <select
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+            className="bg-transparent border border-[#9EA5C4] rounded-[1px] py-1 text-white cursor-pointer focus-visible:outline-none"
+          >
+            <option value="IT">IT</option>
+            <option value="EN">EN</option>
+          </select>
+          
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="p-2">
+            <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 12h16" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 18h16" />
+            </svg>
+          </button>
+        </div>
       </div>
       {isMenuOpen && (
         <nav className="mt-4">
