db54ebda5408e314f5abc1f4971430ef4cceaab6
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-03-02 10:39:57 -0500

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sensus giulietta",
   description: "",
 };
+
+export const geistManrope = Manrope({
+  variable: "--font-geist-manrope",
+  subsets: ["latin"]
+});
 
 export default function RootLayout({
   children,
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
+        className={`${geistManrope.variable} antialiased relative`}
       >
         {children}
       </body>
