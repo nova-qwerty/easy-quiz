1da9ad0af40b44af550f51161981205ecd837424
Jhon Livias <jhon.livias@nova-qwerty.com>
2025-03-14 08:12:52 -0500

    stepper

--- app/components/Stepper/Stepper.tsx
+++ app/components/Stepper/Stepper.tsx
@@ -50,40 +50,44 @@ const Stepper: React.FC<StepperProps> = ({ steps, currentStep, setCurrentStep })
 
   const stepRequiresSelection = ["GRID_IMAGE", "GRID_IMAGE_HORIZONTAL", "GRID_IMAGE_VERTICAL"].includes(steps[currentStep]?.type);
   const isNextDisabled = stepRequiresSelection && !selections[currentStep];
-
+  console.log(`${((currentStep + 1) / (steps.length - 1)) * 100}%`);
   return (
     <div className="flex flex-col items-center pt-[28px] text-white rounded-xl w-[300px] mx-auto">
       <div className="w-[300px]  relative">
-        <div className="relative -top-4 w-full flex justify-between px-2">
-          {steps.slice(0, 6).map((_, index) => (
-            <span
-              key={index}
-              className={`text-[1.5rem] leading-[1.5rem] position-bar position-bar-${index} ${index <= currentStep ? "text-[#9092ad] z-1" : "text-[#b1b4ce]"}`}
-            >
-              |
-            </span>
-          ))}
-        </div>
+      <div className="relative -top-4 w-full flex justify-between px-2">
+        {steps.slice(0, steps.length - 2).map((_, index) => (
+          <span
+            key={index}
+            className={`text-[1.5rem] leading-[1.5rem] position-bar absolute 
+              ${index <= currentStep ? "text-[#9092ad] z-1" : "text-[#b1b4ce]"}`}
+            style={{ left: `${((index + 1) / (steps.length - 1)) * 100}%` }}
+          >
+            |
+          </span>
+        ))}
+      </div>
 
         <div className="w-full h-[10px] bg-bar-color rounded-full overflow-hidden relative border border-[#b1b4ce]">
           <div
             className="h-[10px] bg-bar-color-active transition-all rounded-[5px] bg-bar-border-active mt-[-1px]"
-            style={{ width: `${((currentStep + 1) / 7) * 100}%` }}
+            style={{ width: `${((currentStep + 1) / (steps.length - 1)) * 100 + 1.5}%` }}
           ></div>
         </div>
 
         <div className="absolute top-2 w-full flex justify-between px-2">
           {
-            currentStep == 6 ? 
-              <span className="mr-[auto] ml-[auto] mb-[0] mt-[15px]">{steps[6].last}</span>
+            currentStep ==  steps.length - 2 ? 
+              <span className="mr-[auto] ml-[auto] mb-[0] mt-[15px]">{steps[steps.length - 2].last}</span>
             :
-              currentStep == 7 ?
-                <span className="mr-[auto] ml-[auto] mb-[0] mt-[15px]">{steps[7].last}</span>
+              currentStep ==  steps.length -1 ?
+                <span className="mr-[auto] ml-[auto] mb-[0] mt-[15px]">{steps[ steps.length - 1].last}</span>
               :
-                steps.slice(0, 6).map((_, index) => (
+                steps.slice(0, steps.length - 2).map((_, index) => (
                   <span
                     key={index}
-                    className={`font-medium text-[12px] position-text text-bar-${index} ${index <= currentStep ? "text-white" : "text-gray-400"}`}
+                    className={`font-medium text-[12px] position-text absolute transform -translate-x-1/2
+                      ${index <= currentStep ? "text-white" : "text-gray-400"}`}
+                      style={{ left: `${((index + 1) / (steps.length - 1)) * 100 + 1}%` }}
                   >
                     {index + 1}
                   </span>
